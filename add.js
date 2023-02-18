cfc85ca26e6fdcb50760423ef9f4cd70f6980b20
Ivan <Riadnov.ivan@yandex.ru>
2023-02-18 19:41:38 +0300

    ready

--- add.js
+++ add.js
@@ -1,14 +1,17 @@
 const formEl = document.querySelector("form");
 const tbodyEl = document.querySelector("tbody");
 const tableEl = document.querySelector("table");
-function onAddWebsite(e) {
+function onAddage(e) {
   e.preventDefault();
-  const website = document.getElementById("website").value;
-  const url = document.getElementById("url").value;
+  const age = document.getElementById("age").value;
+  const name = document.getElementById("name").value;
+  const occupation = document.getElementById("occupation").value;
   tbodyEl.innerHTML += `
             <tr>
-                <td>${url}</td>
-                <td>${website}</td>
+                <td>rank</td>
+                <td>${name}</td>
+                <td>${age}</td>
+                <td>${occupation}</td>
                 <td><button class="deleteBtn">Delete</button></td>
             </tr>
         `;
@@ -23,5 +26,5 @@ function onDeleteRow(e) {
   btn.closest("tr").remove();
 }
 
-formEl.addEventListener("submit", onAddWebsite);
+formEl.addEventListener("submit", onAddage);
 tableEl.addEventListener("click", onDeleteRow);
